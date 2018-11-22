a1142ac50a535f4a1a0c0ef68cb3cb4609c4aca1
wolfyuhang <wolfyuhang@163.com>
2018-11-22 16:58:11 +0800

    第一次提交

---
+++ 练习/src/Day07/Scanner的练习.java
@@ -0,0 +1,20 @@
+package Day07;
+
+import java.util.Scanner;
+
+public class Scanner的练习 {
+    public static void main(String[] args) {
+    //调用方法
+//        Scanner sc = new Scanner(System.in);
+//        test(sc);
+    //Day07
+    test(new Scanner(System.in));
+    }
+    public static void test(Scanner sc){
+    //使用sc
+    }
+    //定义一个方法
+    public static Scanner getSanner(){
+        return new Scanner(System.in);
+    }
+}
